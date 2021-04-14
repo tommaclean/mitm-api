2a61bade304a75fc40a973bfae6861321249de7f
tommaclean <tom.j.maclean@gmail.com>
2021-04-13 20:12:25 -0400

    added welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # namespace :api do 
   #   namespace :v1 do 
+      root 'welcome#index'
       post '/login', to: 'auth#create'
       post '/signup', to: 'users#create'
       get '/profile', to:  'users#profile'
